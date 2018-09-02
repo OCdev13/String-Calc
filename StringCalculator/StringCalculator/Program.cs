878b8d91b1ab751e8e8b48362460db8dc037f07e
justin Mckenney <mckenney85@gmail.com>
2018-09-01 22:18:54 -0700

    Step 4: added negative number error handling

--- StringCalculator/StringCalculator/Program.cs
+++ StringCalculator/StringCalculator/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Text.RegularExpressions;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,30 +11,70 @@ namespace StringCalculator
     {
         private static int Add(string nums)
         {
-            Char[] delimeters = {'\n' };
+            // Set a pattern for regex to check for delimiters and push them to an array// 
+            var pattern = @"\//(.*?)\n";
+
+            string delims = Regex.Match(nums, pattern).Groups[1].Value;
+
+            List<string> newDelims = new List<string>
+            {
+                "\n", ","
+            };
             
-            string[] numsArr = nums.Split(delimeters);
+            foreach(Match match in Regex.Matches(nums, delims))
+            {
+                newDelims.Add(match.ToString());
+            }
+          
+            string[] delim = newDelims.ToArray();
+
+            //////////////////////////////////////////////////////////////////////
+
+            //Array for splitting the data//
+            string[] numsArr = nums.Split(delim, StringSplitOptions.RemoveEmptyEntries);
+
+            //Array of negative numbers//
+            List<string> badNums = new List<string>();
+
 
             var a = 0;
 
-            foreach (var num in numsArr)
+            foreach(var num in numsArr)
             {
                 if (numsArr.Length == 0)
                 {
                     return 0;
                 }
+                else if (num.Contains('-'))
+                {
+                        badNums.Add(num);                  
+                }
                 else
                 {
                     Int32.TryParse(num, out int b);
-                    a = a + b; 
+                    a = a + b;
                 }
             }
-            return a; 
+
+
+            // check for negative numbers and throw error if there are any.//
+            if (badNums.Count() != 0)
+            {
+                var message = "";
+                foreach(var b in badNums)
+                {
+                    message += b + " , ";
+                }
+                var e = new Exception("negatives not allowed. " + message);
+                //e.Data["Numbers"] = badNums.ToString();
+                throw e;
+            }
+            return a;
         }
 
         static void Main(string[] args)
         {
-            Console.WriteLine(Add("1\n2\n3\n4\n5"));
+            Console.WriteLine(Add("//;\n1;-2;-3"));
             Console.ReadLine();
         }
 
